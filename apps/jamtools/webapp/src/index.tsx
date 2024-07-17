dee64fb7c047c45527f0e80b0f4edad55055db60
Michael Kochell <6913320+mickmister@users.noreply.github.com>
2024-07-17 03:01:34 -0400

    Basic fullstack app with turborepo and esbuild (#3)
    * basic react app
    
    * turbo and esbuild
    
    * use npx turbo in ci
    
    * basic full stack setup with turbo repo
    
    * add linting and centralized eslint config
    
    * fix eslint command

---
+++ apps/jamtools/webapp/src/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+window.addEventListener('DOMContentLoaded', () => {
+    const rootElem = document.createElement('div');
+    document.body.appendChild(rootElem);
+
+    const root = ReactDOM.createRoot(rootElem);
+    const App = () => (
+        <div>
+            <h1>Jam Tools baby!</h1>
+        </div>
+    );
+
+    root.render(<App />);
+});
