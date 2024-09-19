d75f404d89e1364827472997decc86e150e29316
Michael Kochell <6913320+mickmister@users.noreply.github.com>
2024-09-19 00:48:08 -0400

    remove shoelace mock files

--- packages/jamtools/core/test/shoelace_mock.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const MockComponent = (props) => <div {...props}>Mock Component</div>;
-
-export default MockComponent;
--- packages/jamtools/core/test/shoelace_mock.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const ShoelaceComponentMock = (props: any) => <div {...props}>Mock Component</div>;
-
-export default ShoelaceComponentMock;
