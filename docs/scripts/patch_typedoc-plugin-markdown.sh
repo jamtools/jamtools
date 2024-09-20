f7b14719ac50037b04c65a975649220e374b2aa3
Michael Kochell <6913320+mickmister@users.noreply.github.com>
2024-09-20 00:33:55 -0400

    fix typo for linux

--- docs/scripts/patch_typedoc-plugin-markdown.sh
+++ docs/scripts/patch_typedoc-plugin-markdown.sh
@@ -1,5 +1,5 @@
 sed -i 's/\\//g' "./node_modules/typedoc-plugin-markdown/dist/theme/context/helpers/get-angle-bracket.js"
 
 if grep -q 'typeParameters' ./node_modules/typedoc-plugin-markdown/dist/theme/context/partials/member.signature.js; then
-    sed -i '' '39,75d' ./node_modules/typedoc-plugin-markdown/dist/theme/context/partials/member.signature.js
+    sed -i '39,75d' ./node_modules/typedoc-plugin-markdown/dist/theme/context/partials/member.signature.js
 fi
