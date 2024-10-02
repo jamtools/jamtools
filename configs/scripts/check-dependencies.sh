96c25795ed1c4b1dfec04a2713f78077af980a5f
Michael Kochell <6913320+mickmister@users.noreply.github.com>
2024-10-02 14:39:37 -0400

    edit heroku start script

--- configs/scripts/check-dependencies.sh
+++ configs/scripts/check-dependencies.sh
@@ -1,10 +1,4 @@
-check_and_install() {
-  if [ -d "node_modules/$1" ]; then
-    echo "$1 is installed"
-  else
-    echo "$1 is not installed, installing..."
-    npm install "$1" --omit=dev --no-save
-  fi
-}
-
-check_and_install "jsdom"
+if [ -n "$DYNO" ]; then
+  echo "Running on Heroku, installing any missing production dependencies that may have been pruned"
+  npm install --omit=dev --no-save
+fi
