186eaf48b73aa0da2f02ffd43362a086339118b7
Michael Kochell <6913320+mickmister@users.noreply.github.com>
2024-10-28 00:15:08 -0400

    rpc server changes

--- apps/jamtools/server/src/services/server_json_rpc.ts
+++ apps/jamtools/server/src/services/server_json_rpc.ts
@@ -2,12 +2,18 @@ import express from 'express';
 
 import {WebSocket} from 'ws';
 import {JSONRPCServer, JSONRPCClient, JSONRPCRequest} from 'json-rpc-2.0';
+import {Context} from 'hono';
+import {WSEvents} from 'hono/ws';
+
+type WebsocketInterface = {
+   send: (s: string) => void;
+}
 
 export class NodeJsonRpcServer {
     rpcClient!: JSONRPCClient;
     rcpServer: JSONRPCServer;
 
-    incomingClients: {[clientId: string]: WebSocket} = {};
+    incomingClients: {[clientId: string]: WebsocketInterface} = {};
     outgoingClients: {[clientId: string]: JSONRPCClient} = {};
 
     constructor() {
@@ -15,6 +21,109 @@ export class NodeJsonRpcServer {
     }
 
     maestroClientId = '';
+
+    public handleHonoWebsocketConnection = (c: Context): WSEvents => {
+        let providedClientId = '';
+        let isMaestro = false;
+
+        const incomingClients = this.incomingClients;
+        const outgoingClients = this.outgoingClients;
+
+        const req = c.req;
+
+        if (req.url?.includes('?')) {
+            const urlParams = new URLSearchParams(req.url.substring(req.url.indexOf('?')));
+            providedClientId = urlParams.get('clientId') || '';
+            isMaestro = urlParams.get('is_maestro') === 'true';
+        }
+
+        const clientId = providedClientId || `${Date.now()}`;
+
+        if (isMaestro || !this.maestroClientId) {
+            this.maestroClientId = clientId;
+        }
+
+        let send = (s: string) => { };
+
+        const client = new JSONRPCClient((request: JSONRPCRequest) => {
+            // if (ws.readyState === WebSocket.OPEN) {
+            send(JSON.stringify(request));
+            // ws.send(JSON.stringify(request));
+            return Promise.resolve();
+            // } else {
+            // return Promise.reject(new Error('WebSocket is not open'));
+            // }
+        });
+
+        outgoingClients[clientId] = client;
+
+        return {
+            onOpen: (event, ws) => {
+                incomingClients[clientId] = ws;
+                send = (s: string) => ws.send(s);
+            },
+            onMessage: (event, ws) => {
+                const message = event.data.toString();
+                // console.log(message);
+
+                if (!message) {
+                    return;
+                }
+
+                const jsonMessage = JSON.parse(message);
+                if (!jsonMessage) {
+                    return;
+                }
+
+                // console.log(jsonMessage);
+
+                if (jsonMessage.jsonrpc !== '2.0') {
+                    return;
+                }
+
+                if (!jsonMessage.method) {
+                    // this is a response
+                    const clientId = jsonMessage.clientId as string | undefined;
+                    if (clientId) {
+                        incomingClients[clientId]?.send(message);
+                    }
+
+                    return;
+                }
+
+                if (clientId !== this.maestroClientId) {
+                    incomingClients[this.maestroClientId]?.send(message);
+                    return;
+                }
+
+                if (jsonMessage.id) {
+                    // console.log('this shouldnt happen')
+                    return;
+                }
+
+                // broadcast message
+
+                for (const c of Object.keys(incomingClients)) {
+                    if (c === clientId) {
+                        continue;
+                    }
+
+                    const method = jsonMessage.method;
+                    const args = jsonMessage.params;
+
+                    incomingClients[c]?.send(message);
+
+                    // const result = await outgoingClients[c].request(method, args);
+                    // client.send(result);
+                }
+            },
+            onClose: () => {
+                delete incomingClients[clientId];
+                delete outgoingClients[clientId];
+                console.log('Connection closed');
+            },
+        };
+    };
 
     handleConnection = async (ws: WebSocket, req: express.Request) => {
         let providedClientId = '';
